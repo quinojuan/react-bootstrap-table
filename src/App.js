3f6de5ba6c9aee67093dc3c9b780d15aaa0798bc
Juan Quino <quinojuan@gmail.com>
2023-03-09 15:20:05 -0300

    funciones nuevas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import BootstrapTable from "react-bootstrap-table-next";
+import paginationFactory from "react-bootstrap-table2-paginator";
+import cellEditFactory, { Type } from "react-bootstrap-table2-editor";
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    axios("https://jsonplaceholder.typicode.com/comments").then((res) => {
+      console.log(res.data);
+      setData(res.data);
+    });
+  };
+
+  const emailFormatter = (data, row) => {
+    return <span>Email: {data}</span>;
+  };
+  const columns = [
+    {
+      dataField: "email",
+      text: "Email",
+      sort: true,
+      formatter: emailFormatter,
+    },
+    {
+      dataField: "postId",
+      text: "Product ID",
+      sort: true,
+    },
+    {
+      dataField: "name",
+      text: "Name",
+      sort: true,
+    },
+  ];
   return (
-    <div className="App">
-      <h1>Hello World from App</h1>
+    <div>
+      <BootstrapTable
+        keyField="id"
+        data={data}
+        columns={columns}
+        striped
+        hover
+        condensed
+        pagination={paginationFactory()}
+        cellEdit={cellEditFactory({
+          mode: "click",
+        })}
+      />
     </div>
   );
-}
+};
 
 export default App;
